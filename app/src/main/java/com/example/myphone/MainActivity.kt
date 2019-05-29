9a3fad262d556342c1b07d462c8eb256f84a040b
Lipatov <YLipatov@ad.harman.com>
2019-05-29 17:55:12 +0300

    first commit

---
+++ app/src/main/java/com/example/myphone/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myphone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.widget.Toolbar
+import android.view.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav:BottomNavigationView
+    lateinit var tool:Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.navigation)
+        tool = findViewById(R.id.myToolbar)
+        setSupportActionBar(tool)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         menuInflater.inflate(R.menu.search_contacts,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
